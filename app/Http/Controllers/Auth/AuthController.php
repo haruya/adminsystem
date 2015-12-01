d0d4b2e81bed81e986a4f9a7ad523e77b2bdf275
haruya ichikawa <ichikawa@sonar-enterprise.com>
2015-12-01 18:27:33 +0900

    first commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,84 @@
+<?php namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\Registrar;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Registration & Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users, as well as the
+	| authentication of existing users. By default, this controller uses
+	| a simple trait to add these behaviors. Why don't you explore it?
+	|
+	*/
+
+	use AuthenticatesAndRegistersUsers;
+
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
+	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
+	 * @return void
+	 */
+	public function __construct(Guard $auth, Registrar $registrar)
+	{
+		$this->auth = $auth;
+		$this->registrar = $registrar;
+
+		$this->middleware('guest', ['except' => 'getLogout']);
+	}
+
+	/**
+	 * ユーザ作成画面表示
+	 */
+	public function getCreate()
+	{
+		return view('auth.create');
+	}
+
+	/**
+	 * ユーザ新規追加処理
+	 */
+	public function postCreate(Request $request)
+	{
+		// バリデーション
+		$val = $this->validator($request->all());
+		if ($val->fails()) {
+			return redirect()->intended($this->redirectPath())->withInput();
+		}
+		$user = $this->create($request->all());
+		return redirect()->to('home');
+	}
+
+	/**
+	 * バリデーション
+	 */
+	protected function validator(array $data)
+	{
+		return Validator::make($data, [
+			'name' => 'required|max:255',
+			'email' => 'required|email|max:255|unique:users',
+			'password' => 'required|confirmed|min:8',
+		]);
+	}
+
+	/**
+	 * ユーザ新規追加
+	 */
+	protected function create(array $data)
+	{
+		return User::create([
+			'name' => $data['name'],
+			'email' => $data['email'],
+			'password' => bcrypt($data['password']),
+		]);
+	}
+}
---
+++ resources/views/tbs/base.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>@yield('title')</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+
+  <!-- Fonts -->
+  <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+  @section('navbar')
+  @show
+  @section('sidebar')
+  @show
+  <div class="container" style="margin-top: 30px;">
+    @yield('content')
+  </div>
+  @section('footer')
+  @show
+  <!-- Scripts -->
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
